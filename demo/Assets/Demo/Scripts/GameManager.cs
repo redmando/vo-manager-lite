a26a549fb9e13a4586ae97724e761717d4102986
tvledesign <tvledesign@gmail.com>
2017-12-22 02:27:06 -0600

    Updated Door Conversation
    Updated door conversation to Play regularly and Force Play

--- demo/Assets/Demo/Scripts/GameManager.cs
+++ demo/Assets/Demo/Scripts/GameManager.cs
@@ -4,14 +4,20 @@ using System.Collections;
 public class GameManager : MonoBehaviour
 {
     // global variables
-    public static GameManager Instance; // this instance
-    public float fltDelayBetweenAudio = 1.0f;   // the delay between audio files
+    public static GameManager Instance; // this instance  
 
     // public variables
+    [Header("Settings")]
+    public bool blnForcePlay;   // check if the current mode is force play mode
+
     [Header("Door")]
+    public AudioSource audSrcDoor;   // the audio source for the door conversation
     public bool blnTriggerDoor; // check if the door conversation is triggerable
     public bool blnDoorConvoStart;  // check if the door conversation has started
+    public float fltDelayBetweenAudioDoor = 1.0f;   // the delay between audio files
     public int[] col_intHangingPictureConvo;  // an array to store the order for the hanging picture conversation
+
+
 
     // private variables
     private int m_intCurrentIndex;  // keep track of the current audio's index
@@ -24,56 +30,85 @@ public class GameManager : MonoBehaviour
         Instance = this;
     }
 
-	// update is called once per frame
-	void Update ()
+    // update is called once per frame
+    void Update()
     {
-	    // if the door trigger is on
-        if(blnTriggerDoor)
+        // if the door trigger is on
+        if (blnTriggerDoor)
         {
             // if the door conversation has not started
-            if(!blnDoorConvoStart)
+            if (!blnDoorConvoStart)
             {
                 // set the text to inform the user they can trigger the conversation
                 VOManager.Instance.uiTextObject.text = "Press [E] to listen to the conversation in the hallway.";
             }
 
-            // if the door conversation has started 
-            if(blnDoorConvoStart)
+            // if the E key is pressed
+            if (Input.GetKeyDown(KeyCode.E))
             {
-                // if the audio source isn't playing
-                if(!VOManager.Instance.IsPlaying())
+                // if the door conversation has not started
+                if (!blnDoorConvoStart)
                 {
-                    // if the current index is less than the list length
-                    if (m_intCurrentIndex < col_intHangingPictureConvo.Length)
-                    {
-                        // if the delay between audio files is 0
-                        if (m_fltDelayBetweenAudio <= 0)
-                        {
-                            // play the first audio and increment the index
-                            VOManager.Instance.Play(col_intHangingPictureConvo[m_intCurrentIndex]);
-                            m_intCurrentIndex++;
+                    // start the convo and set index to 0
+                    blnDoorConvoStart = true;
+                    m_intCurrentIndex = 0;
 
-                            // set the default delay
-                            m_fltDelayBetweenAudio = fltDelayBetweenAudio;
-                        } else
-                        {
-                            // decrease the delay based on time
-                            m_fltDelayBetweenAudio -= Time.deltaTime;
-                        }
-                    }
+                    // set the delay between audio
+                    m_fltDelayBetweenAudio = 0;
+                }
+
+                // if the current mode is force play mode
+                if(blnForcePlay)
+                {
+                    // force play the first audio clip and set the index to 1
+                    blnDoorConvoStart = true;
+                    VOManager.Instance.ForcePlay(audSrcDoor, col_intHangingPictureConvo[0]);
+                    m_intCurrentIndex = 1;
+
+                    // set the delay between audio
+                    m_fltDelayBetweenAudio = fltDelayBetweenAudioDoor;
                 }
             }
+        }
 
-            // if the E key is pressed
-            if(Input.GetKeyDown(KeyCode.E))
+        // if the door conversation has started 
+        if (blnDoorConvoStart)
+        {
+            // if the audio source isn't playing
+            if (!VOManager.Instance.IsPlaying())
             {
-                // start the convo and set index to 0
-                blnDoorConvoStart = true;
-                m_intCurrentIndex = 0;
+                // if the current index is less than the list length
+                if (m_intCurrentIndex < col_intHangingPictureConvo.Length)
+                {
+                    // if the delay between audio files is 0
+                    if (m_fltDelayBetweenAudio <= 0)
+                    {
+                        // play the audio at the audio source and increment the index
+                        VOManager.Instance.Play(audSrcDoor, col_intHangingPictureConvo[m_intCurrentIndex]);
+                        m_intCurrentIndex++;
 
-                // set the delay between audio
-                m_fltDelayBetweenAudio = 0;
+                        // set the default delay
+                        m_fltDelayBetweenAudio = fltDelayBetweenAudioDoor;
+                    }
+                    else
+                    {
+                        // decrease the delay based on time
+                        m_fltDelayBetweenAudio -= Time.deltaTime;
+                    }
+                }
+                else
+                {
+                    // set convo to false
+                    blnDoorConvoStart = false;
+                }
             }
         }
-	}
+
+        // if there are no triggers
+        if (!blnTriggerDoor && !blnDoorConvoStart)
+        {
+            // set the text to an empty string
+            VOManager.Instance.uiTextObject.text = "";
+        }
+    }
 }
