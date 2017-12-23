8b0af462616c1d9259907ba2115d2eb9b9655364
tvledesign <tvledesign@gmail.com>
2017-12-23 03:53:09 -0600

    Added FaceCamera Functionality

---
+++ demo/Assets/Demo/Scripts/FaceCamera.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class FaceCamera : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.LookAt(Camera.main.transform);
+    }
+}
