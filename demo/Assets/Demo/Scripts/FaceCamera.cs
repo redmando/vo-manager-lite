262800717d8bbf1bac8b39545b96a40a9b789182
tvledesign <tvledesign@gmail.com>
2017-12-23 22:18:35 -0600

    Added Comments to FaceCamera.cs

--- demo/Assets/Demo/Scripts/FaceCamera.cs
+++ demo/Assets/Demo/Scripts/FaceCamera.cs
@@ -1,11 +1,16 @@
 ﻿using UnityEngine;
 
+/// <summary>
+/// Face Camera
+/// Description: Have an object always face the camera.
+/// </summary>
+
 public class FaceCamera : MonoBehaviour
 {
     // Update is called once per frame
     void Update()
     {
-
+        // face the camera
         transform.LookAt(Camera.main.transform);
     }
 }
